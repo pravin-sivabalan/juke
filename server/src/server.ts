dc26a60778b6b9e8f7d350f8d17297a64cf82a37
Pravin Sivabalan <pravinsivabalan@gmail.com>
2017-07-02 13:23:37 -0500

    Move socket logic to different file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,25 @@
 import * as http from 'http';
-import {Express} from 'express';
-import {app} from './routes/index';
-import {properties} from './utils/properties';
 import * as mongoose from 'mongoose';
+import {app} from './routes/index';
+import {Express} from 'express';
+import {properties} from './utils/properties';
+import {Socket} from './sockets/index';
 
 
 class Server {
   public _app: Express;
   public _server: http.Server;
-  public _io :any;
+  public _socket :any;
 
   constructor() {
     this._app = app;
     this._server = http.createServer(this._app);
+    this._socket = new Socket(this._server);
   }
 
   start() :void {
     this._server.listen(3000);
-    console.log('Server listening on port 3000')
+    console.log('Server listening on port 3000');
 
     mongoose.connect(properties.database.mongodb);
   }
