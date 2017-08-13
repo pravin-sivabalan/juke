829f7b9bf48b863c51c08cae2b8ffcae52291ef7
Pravin Sivabalan <pravinsivabalan@gmail.com>
2017-08-12 20:28:22 -0500

    Add player service

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerService } from '../shared/services/player.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
-  providers: []
+  providers: [PlayerService]
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private playerService: PlayerService) { }
 
   ngOnInit() { }
+
+  play() {
+    this.playerService.play().subscribe();
+  }
 
 }
