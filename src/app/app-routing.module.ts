c2171b565aa975797f703093a0901a684b21342f
Pravin Sivabalan <pravinsivabalan@gmail.com>
2017-08-12 20:27:59 -0500

    Add auth guard to dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 import { DashboardRoutes } from './dashboard/dashboard.routes';
 
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  ...DashboardRoutes
+  { path: '', redirectTo: '/dash', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  ...DashboardRoutes,
+  {path: '404', component: NotFoundComponent},
+  {path: '**', redirectTo: '/404'}
 ];
 
 @NgModule({
